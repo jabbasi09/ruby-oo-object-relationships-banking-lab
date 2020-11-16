818e3e42dbc08d09cc493aaeb24a6f8a7a32f422
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-15 19:54:34 -0600

    commit tree 2b6069aadfbec03c02fb8ea7f85fb6faf7ebc054

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+    attr_accessor :bankaccount, :balance, :status
+    attr_reader :name
+    def initialize(bankaccount)
+        @bankaccount = bankaccount
+        @name = "Avi"
+        @balance = 1000
+        @status = "open"
+    end
 
+    def deposit(money)
+        self.balance = 2000
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
